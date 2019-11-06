4c900a81d902b82fff30a9898a404ce1c00c72c3
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-11-05 19:17:05 -0500

    Create useDestructiveReadMap hook, for single-time reads

--- src/App.js
+++ src/App.js
@@ -51,6 +51,37 @@ function useKeyboard(audioContext, voice) {
   ]
 }
 
+function useDestructiveReadMap(initialState) {
+  const [map, setMap] = useState(initialState);
+
+  const put = (key, value) => {
+    setMap((m) => {
+      const update = {};
+      update[key] = value;
+      const withUpdate = Object.assign({}, m, update);
+      return withUpdate;
+    });
+  };
+
+  const read = (key) => {
+    var value;
+
+    setMap((map) => {
+      value = map[key];
+      const withoutKey = Object.assign({}, map);
+      delete withoutKey[key];
+      return withoutKey;
+    });
+
+    return value;
+  };
+
+  return [
+    put,
+    read,
+  ];
+}
+
 function useKeyboardMonitor(onPress, onRelease) {
   const [keysDownCurrently, setKeysDownCurrently] = useState([]);
 
