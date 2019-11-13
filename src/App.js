aa4c555acc8d5dac6f384ffa178fe1655119f1cf
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-11-12 21:22:22 -0500

    Extract layout as a prop

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useRef, useState, useEffect } from "react";
 import { Note } from "@jonathanhunsucker/music-js";
 import { Gain, Envelope, Wave, silentPingToWakeAutoPlayGates } from "@jonathanhunsucker/audio-js";
 
-import Keyboard from "./Keyboard.js";
+import { key, offset, Keyboard } from "./Keyboard.js";
 import Patch from "./Patch.js";
 import { Mapping, Handler } from "./KeyCommand.js";
 import useKeystrokeMonitor from "./useKeystrokeMonitor.js";
@@ -152,12 +152,72 @@ function App() {
 
   useKeystrokeMonitor(onPress, onRelease);
 
+  const layout = [
+    [
+      key("Digit1"),
+      key("Digit2"),
+      key("Digit3"),
+      key("Digit4"),
+      key("Digit5"),
+      key("Digit6"),
+      key("Digit7"),
+      key("Digit8"),
+      key("Digit9"),
+      key("Digit0"),
+      key("Minus"),
+      key("Equal"),
+    ],
+    [
+      offset(0.5),
+      key("KeyQ"),
+      key("KeyW"),
+      key("KeyE"),
+      key("KeyR"),
+      key("KeyT"),
+      key("KeyY"),
+      key("KeyU"),
+      key("KeyI"),
+      key("KeyO"),
+      key("KeyP"),
+      key("BracketLeft"),
+      key("BracketRight"),
+    ],
+    [
+      offset(0.8),
+      key("KeyA"),
+      key("KeyS"),
+      key("KeyD"),
+      key("KeyF"),
+      key("KeyG"),
+      key("KeyH"),
+      key("KeyJ"),
+      key("KeyK"),
+      key("KeyL"),
+      key("Semicolon"),
+      key("Quote"),
+    ],
+    [
+      offset(1),
+      key("KeyZ"),
+      key("KeyX"),
+      key("KeyC"),
+      key("KeyV"),
+      key("KeyB"),
+      key("KeyN"),
+      key("KeyM"),
+      key("Comma"),
+      key("Period"),
+      key("Slash"),
+      key("ShiftRight", 1.5),
+    ],
+  ];
+
   return (
     <div className="App">
       <h1>Keyboard</h1>
       <p>Shift: {shift}</p>
       <p>Level: <input type="range" min="0" step="0.01" max="1.0" value={level} onChange={(e) => {setLevel(e.target.valueAsNumber)}} /></p>
-      <Keyboard mapping={mapping} pressed={keysDownCurrently} onPress={onPress} onRelease={onRelease} />
+      <Keyboard layout={layout} mapping={mapping} pressed={keysDownCurrently} onPress={onPress} onRelease={onRelease} />
       <br />
       <Patch patch={patch} setPatch={setPatch} />
     </div>
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -28,14 +28,14 @@ function label(mapping, pressed, code) {
   return <b>{mapping.label(code)}</b>;
 }
 
-function key(code, span) {
+export function key(code, span) {
   return {
     code: code,
     span: span || 1,
   };
 }
 
-function offset(span) {
+export function offset(span) {
   return key(null, span);
 }
 
@@ -81,70 +81,10 @@ function square(mapping, pressed, onPress, onRelease, key) {
   );
 }
 
-export default function Keyboard(props) {
-  const layout = [
-    [
-      key("Digit1"),
-      key("Digit2"),
-      key("Digit3"),
-      key("Digit4"),
-      key("Digit5"),
-      key("Digit6"),
-      key("Digit7"),
-      key("Digit8"),
-      key("Digit9"),
-      key("Digit0"),
-      key("Minus"),
-      key("Equal"),
-    ],
-    [
-      offset(0.5),
-      key("KeyQ"),
-      key("KeyW"),
-      key("KeyE"),
-      key("KeyR"),
-      key("KeyT"),
-      key("KeyY"),
-      key("KeyU"),
-      key("KeyI"),
-      key("KeyO"),
-      key("KeyP"),
-      key("BracketLeft"),
-      key("BracketRight"),
-    ],
-    [
-      offset(0.8),
-      key("KeyA"),
-      key("KeyS"),
-      key("KeyD"),
-      key("KeyF"),
-      key("KeyG"),
-      key("KeyH"),
-      key("KeyJ"),
-      key("KeyK"),
-      key("KeyL"),
-      key("Semicolon"),
-      key("Quote"),
-    ],
-    [
-      offset(1),
-      key("KeyZ"),
-      key("KeyX"),
-      key("KeyC"),
-      key("KeyV"),
-      key("KeyB"),
-      key("KeyN"),
-      key("KeyM"),
-      key("Comma"),
-      key("Period"),
-      key("Slash"),
-      key("ShiftRight", 1.5),
-    ],
-  ];
-
+export function Keyboard(props) {
   return (
     <div>
-      {layout.map((row, i) => {
+      {props.layout.map((row, i) => {
         return (
           <div key={i} style={{overflow: "auto", with: "100%"}}>
             {row.map((item, j) => {
